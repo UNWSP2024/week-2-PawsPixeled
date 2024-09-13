a00f0986c5cab649151f21d5e820fe2b9165bf55
Caiden <98279099+PawsPixeled@users.noreply.github.com>
2024-09-13 17:49:54 -0500

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -25,3 +25,15 @@ print(phone,'is a great phone number')
 #Collects the Collge Major
 major = input('What is your college major? ')
 print(major,',that sounds interesting')
+
+#/Users/caidennelson/PycharmProjects/PythonProgramming/.venv/bin/python /Users/caidennelson/PycharmProjects/PythonProgramming/.venv/week 2/Personal Info.py 
+#What is your name? Caiden
+#hello Caiden
+##Please write in order what city and state you live in then your ZIP code Ooga Booga, MN, 29835
+#Your address is Ooga Booga, MN, 29835
+#What is your phone number? 1234567891
+#1234567891 is a great phone number
+#What is your college major? Something
+#Something ,that sounds interesting
+
+#Process finished with exit code 0
