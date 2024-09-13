3855f8cb87fee383989c4f2ca7e6c59a73b18c18
Caiden <98279099+PawsPixeled@users.noreply.github.com>
2024-09-13 16:43:12 -0500

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,6 +1,27 @@
-def personal_information():
-    print('...')
+#def personal_information():
+ #   print('...')
 
 
 # Line which calls the above function.
-personal_information()
+#personal_information()
+
+
+#Personal info stealer
+#Programmer = Caiden Nelson
+#Date created 9/13/24
+
+#Collects the name
+name = input('What is your name? ')
+print( 'hello',name )
+
+#Collects the address
+Address = input('Please write in order what city and state you live in then your ZIP code ')
+print('Your address is',Address)
+
+#Collects the Phone Number
+phone = int(input('What is your phone number? '))
+print(phone,'is a great phone number')
+
+#Collects the Collge Major
+major = input('What is your college major? ')
+print(major,',that sounds interesting')
